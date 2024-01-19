ca7bca87bf04ce3bfb519500daa0d367ba9bb21a
TrungNQ <trungnq@ltsgroup.tech>
2024-01-19 10:15:13 +0700

    add api

--- [DATN] - SaleQQ/Controllers/AuthController.cs
+++ [DATN] - SaleQQ/Controllers/AuthController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using _DATN____SaleQQ.Dto.Request.AuthRequest;
+using _DATN____SaleQQ.Service;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace _DATN____SaleQQ.Controllers
@@ -7,5 +10,59 @@ namespace _DATN____SaleQQ.Controllers
     [ApiController]
     public class AuthController : ControllerBase
     {
+        private readonly AuthService _authService;
+
+        public AuthController(AuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> login(LoginRequest loginRequest)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var result = await _authService.Login(loginRequest);
+            return result.Success ? Ok(result) : BadRequest(result);
+        }
+
+        [HttpPost("logout")]
+        public async Task<IActionResult> Logout()
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            await HttpContext.SignOutAsync();
+
+            var result = await _authService.Logout();
+
+            return result.Success ? Ok(result) : BadRequest(result);
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(RegisterRequest registerRequest)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var result = await _authService.Register(registerRequest);
+            return result.Success ? Ok(result) : BadRequest(result);
+        }
+
+        [HttpGet("confirm-email")]
+        public async Task<IActionResult> ConfirmEmail(ConfirmEmailRequest ConfirmEmailRequest)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var result = await _authService.ConfirmEmail(ConfirmEmailRequest);
+            return result.Success ? Ok(result) : BadRequest(ConfirmEmailRequest);
+        }
     }
 }
